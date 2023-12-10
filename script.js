cdd57915ac5f154cf8f6b93738532a88951627e6
David BANITONGWA <davidbanitongwa@gmail.com>
2023-12-10 19:14:20 +0100

    update 1 file and create 1 file

---
+++ script.js
@@ -0,0 +1,26 @@
+const end = Date.now() + 15 * 1000;
+
+// go Buckeyes!
+const colors = ["#bb0000", "#ffffff"];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
